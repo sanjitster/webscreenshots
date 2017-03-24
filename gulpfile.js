2fcfc4cb1be86299451fecfab66f0242cdfd995b
Santhosh Kuma <sanjitster@gmail.com>
2017-03-24 12:23:38 +0800

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,119 @@
+var gulp = require('gulp'),
+    fs = require('fs-extra'),
+    parsePath = require('parse-filepath'),
+    del = require('del'),
+    webshot = require('webshot');
+
+// Config File
+var config = JSON.parse(fs.readFileSync('config.json'));
+
+// Clean screens directory
+gulp.task('clean:screens', function () {
+    del.sync(['screens/']);
+});
+
+// Build screenshots from extrasmall devices (<768px)
+gulp.task('screens:extrasmall', function () {
+    var options = {
+        screenSize: {
+            width: 375,
+            // height: 600
+        },
+        shotSize: {
+            width: 375,
+            height: 'all'
+        }
+    };
+    var f = parsePath(config.url);
+    if (f.extname === '.html') {
+        webshot(
+            config.url,
+            'screens/extrasmall-device-375.jpg',
+            options,
+            function () { //err
+                // console.log(err);
+            }
+        );
+    }
+});
+
+// Build screenshots from small devices (≥768px)
+gulp.task('screens:small', function () {
+    var options = {
+        screenSize: {
+            width: 768,
+            // height: 600
+        },
+        shotSize: {
+            width: 768,
+            height: 'all'
+        }
+    };
+    var f = parsePath(config.url);
+    if (f.extname === '.html') {
+        webshot(
+            config.url,
+            'screens/small-device-768.jpg',
+            options,
+            function () { //err
+                // console.log(err);
+            }
+        );
+    }
+});
+
+// Build screenshots from medium devices (≥992px)
+gulp.task('screens:medium', function () {
+    var options = {
+        screenSize: {
+            width: 900  ,
+            // height: 600
+        },
+        shotSize: {
+            width: 900,
+            height: 'all'
+        }
+    };
+    var f = parsePath(config.url);
+    if (f.extname === '.html') {
+        webshot(
+            config.url,
+            'screens/medium-device-992.jpg',
+            options,
+            function () { //err
+                // console.log(err);
+            }
+        );
+    }
+});
+
+// Build screenshots from large devices (≥992px)
+gulp.task('screens:large', function () {
+    var options = {
+        screenSize: {
+            width: 1200,
+            // height: 600
+        },
+        shotSize: {
+            width: 1200,
+            height: 'all'
+        }
+    };
+    var f = parsePath(config.url);
+    if (f.extname === '.html') {
+        webshot(
+            config.url,
+            'screens/large-device-1200.jpg',
+            options,
+            function () { //err
+                // console.log(err);
+            }
+        );
+    }
+});
+
+// Build screenshots from web into public folder
+gulp.task('screenshots', ['clean:screens', 'screens:extrasmall', 'screens:small', 'screens:medium', 'screens:large']);
+
+// Default gulp tasks
+gulp.task('default', ['screenshots']);
